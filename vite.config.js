99e13343b72855a301e9094ef2d969d6b79753d9
Serg310583 <serg310583@mail.ru>
2024-11-05 20:50:42 +0700

    add deploy firebase

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tele2-test/',
+  // base: '/t2-test',
   plugins: [react()],
   // Директория, в которой находятся статические файлы
   publicDir: 'public',
+  build: {
+    outDir: 'dist',
+  },
   css: {
     preprocessorOptions: {
-      
       scss: {
         api: 'modern-compiler', // or "modern", "legacy"
-        
       },
     },
   },
